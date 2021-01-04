64d186a3c7afea8b1f5c5c61c441a9ee6a8e73e4
Alexanderjansson91 <alexander_jansson_91@hotmail.com>
2021-01-04 17:02:52 +0100

    added and header and an textfeild

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Text, TextInput, View } from 'react-native';
+import Header from './src/component/header'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello dude!!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.topContainer}>
+      <Header headerText="Hello!" infoText="this is my first React Native Expo App" />
+      <View style={styles.ViewContainer}>
+        <Text style={styles.infoText}>My goals for this course</Text>
+        <TextInput style={styles.inputTextfield}>
+        </TextInput>
+    </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+
+  topContainer: {
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  ViewContainer: {
+    backgroundColor: '#D3D3D3',
+    flex: 1
+  },
+  inputTextfield: {
+    backgroundColor: '#ffffff',
+    height:100,
+    width:350,
+    borderRadius:10,
+    marginLeft: 20,
+    marginTop:20, 
+  },
+
+  infoText: {
+    marginLeft: 20,
+    marginTop:40,
+    
+  },
+
 });
---
+++ src/component/header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Header = (props)=> {
+    const {textContainer, viewContainer, infoText} = styles;
+    return(
+        <View style={viewContainer}>
+            <Text style={textContainer}>
+                {props.headerText}
+            </Text>
+            <Text style={infoText}>
+                {props.infoText}
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    viewContainer:{
+       backgroundColor: '#800080'
+    },
+    textContainer:{
+        fontSize:30,
+        marginTop: 60, 
+        marginLeft: 20,
+        color:"#ffffff",
+    },
+    infoText:{
+        fontSize:15,
+        marginTop: 10, 
+        marginLeft: 20,
+        color:"#ffffff",
+        marginBottom: 40, 
+    },
+
+});
+
+export default Header;
